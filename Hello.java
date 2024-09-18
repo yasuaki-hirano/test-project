3744540f4b4983e76582db3c0d04732d4ab2ee26
daiki_windows <dddaiki3150@gmail.com>
2024-09-18 16:55:17 +0900

    modify Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
